a5ad74ed8493d42daca77ba633fb650f8fc5e625
Stefano Cipriani <stefano.voce@gmail.com>
2020-07-02 22:56:31 +0200

    Aggiunto gestore eccezioni per test B_ErrlistArtByEan()

--- src/main/java/com/xantrix/webapp/controller/ArticoliController.java
+++ src/main/java/com/xantrix/webapp/controller/ArticoliController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.xantrix.webapp.entities.Articoli;
 import com.xantrix.webapp.entities.Barcode;
+import com.xantrix.webapp.exception.NotFoundException;
 import com.xantrix.webapp.service.BarcodeService;
 
 @RestController
@@ -25,15 +26,17 @@ public class ArticoliController {
 	
 	@GetMapping(value = "cerca/ean/{barcode}", produces = "application/json")
 	public ResponseEntity<Articoli> listArtByEan(@PathVariable("barcode") String barcode)
+		throws NotFoundException
 	{
 		logger.info("********** Otteniamo l'articolo con barcode: {} **********", barcode);
 		Articoli articolo = null;
 		Barcode ean = barcodeService.selByBarcode(barcode);
 		
 		if(ean == null) {
-			String errMsg = String.format("il barcode %s non è stato trovato", barcode);
+			String errMsg = String.format("Il barcode %s non è stato trovato!", barcode);
 			logger.warn(errMsg);
-			return new ResponseEntity<Articoli>(HttpStatus.NOT_FOUND);
+			throw new NotFoundException(errMsg);
+			//return new ResponseEntity<Articoli>(HttpStatus.NOT_FOUND);
 		}else {
 			articolo = ean.getArticolo();
 			return new ResponseEntity<Articoli>(articolo,HttpStatus.OK);
